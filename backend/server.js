42928134a61f4573037fc1cf0acb27af40baa320
sohamdasx <sohamdas.2702@gmail.com>
2024-06-29 22:42:21 +0530

    initialized

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const workoutRoutes = require("./routes/workouts")
+
+// express app
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("listening on port");
+});
