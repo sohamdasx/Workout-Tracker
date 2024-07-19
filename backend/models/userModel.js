c60d7443a5232853ef178688c5bf7ca13204fbfc
sohamdasx <sohamdas.2702@gmail.com>
2024-07-19 16:09:51 +0530

    :D

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,13 +20,13 @@ const userSchema = new Schema({
 userSchema.statics.signup = async function (email, password) {
   // validation
   if (!email || !password) {
-    throw Error("All fields must be filled.");
+    throw Error("All fields must be filled");
   }
   if (!validator.isEmail(email)) {
-    throw Error("Email is not valid.");
+    throw Error("Email not valid");
   }
   if (!validator.isStrongPassword(password)) {
-    throw Error("Password not strong enough.");
+    throw Error("Password not strong enough");
   }
 
   const exists = await this.findOne({ email });
@@ -44,22 +44,19 @@ userSchema.statics.signup = async function (email, password) {
 };
 
 // static login method
-
 userSchema.statics.login = async function (email, password) {
   if (!email || !password) {
-    throw Error("All fields must be filled.");
+    throw Error("All fields must be filled");
   }
 
   const user = await this.findOne({ email });
-
   if (!user) {
-    throw Error("Incorrect email.");
+    throw Error("Incorrect email");
   }
 
   const match = await bcrypt.compare(password, user.password);
-
   if (!match) {
-    throw Error("Incorrect password.");
+    throw Error("Incorrect password");
   }
 
   return user;
